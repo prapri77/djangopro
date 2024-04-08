e26d307cc1487408ec678d752f43d80556b31404
prapri77 <prasanthmaster77@gmail.com>
2024-04-09 03:15:04 +0530

    project

---
+++ responsive-website/pythonProject/responsivepro/myapp/static/js/script.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function(){
+      // Auto play carousel
+      $('.carousel').carousel({
+        interval: 1000 // Change interval (time in milliseconds)
+      });
+    });
+ 
